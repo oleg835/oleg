3f4524b149a0bac7f146b15df86d7cb0e456b6e2
oleg835 <36995001+oleg835@users.noreply.github.com>
2018-03-06 19:09:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+const Cat = mongoose.model('Cat', { name: String });
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    // we're connected!
+});
 var xxx = express();
 xxx.use(express.static(path.join(__dirname, 'public')));
 var bodyParser = require('body-parser');
@@ -11,4 +23,6 @@ xxx.use(bodyParser.urlencoded({
 }));
 xxx.get('/', function(req,res){res.render('index.html')});
 
-xxx.listen(PORT,function(){ console.log(`Listening on ${ PORT }`)});
+xxx.listen(5000, function(){
+    console.log('listening on *:5000');
+});
